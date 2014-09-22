a07ddff67f182a9706beb6c73fc49b93a715e6ef
helllth <th@cismet.de>
2014-09-22 11:59:44 +0200

    Initial commit

---
+++ exports.sh
@@ -0,0 +1,25 @@
+export M2_HOME=/Users/thorsten/bin/maven/3.0.4/
+export M2=$M2_HOME/bin
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home"
+export JVMSTAT_JAVA_HOME=$JAVA_HOME
+#export JAVA_HOME=/Library/Java/Home/
+#export JAVA_HOME="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/"
+
+export PATH=/usr/local/sbin:/usr/local/bin:$PATH:$M2
+export PATH=/Library/Frameworks/GDAL.framework/Programs:$PATH
+export PATH=$PATH:/Users/thorsten/Development/glassfish/bin:/Users/thorsten/bin:/usr/local/share/npm/bin
+export SVN_EDITOR=/usr/bin/vim
+export EDITOR=/usr/bin/vim
+export PYTHONPATH=/usr/local/lib/python:$PYTHONPATH
+export HOMEBREW_TEMP=/tmp
+export NODE_PATH=/usr/local/lib/node_modules
+export MAVEN_OPTS="-Djava.awt.headless=true -Xmx1024m"
+#export JAVA_OPTS="-XDfile.encoding=UTF-8"
+export PATH=/usr/local/sbin:/usr/local/bin:$PATH
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWSTASHSTATE=true 
+export GIT_PS1_SHOWUNTRACKEDFILES=true 
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export LC_CTYPE="en_US.UTF-8"
